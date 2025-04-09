14a193e895b51fd59fcefb6bfde7f62bd69fe197
¨Camilo <camiloleon981@gmail.com>
2025-04-08 21:53:49 -0500

    tablas de verdad

--- ejercicios.js
+++ ejercicios.js
@@ -5,15 +5,3 @@ En informática una cola (queue) es una estructura de datos abstracta en la cual
 Define una función proximoEnLaFila que tome un arreglo (arreglo) y un número (elemento) como argumentos. Agrega el número al final del arreglo y luego elimina el primer elemento del arreglo. La función proximoEnLaFila debe retornar el elemento que fue removido.
 
 */
-function proximoEnLaFila(arreglo, elemento) {
-    arreglo.push(elemento);
-    return arreglo.shift();
-}
-
-console.log(" A     | B     | A && B ");
-console.log("-------------------------");
-console.log(" false | false |", false && false);
-console.log(" false | true  |", false && true);
-console.log(" true  | false |", true && false);
-console.log(" true  | true  |", true && true);
-
